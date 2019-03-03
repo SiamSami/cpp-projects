0fc79795a3aff4a9e6e74b10838b907292323b61
SiamSami <33563064+SiamSami@users.noreply.github.com>
2019-03-03 10:02:11 +0600

    Create matrix.cpp

---
+++ matrix.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <unistd.h>
+using namespace std;
+int main(){
+	int random;
+	string alpha[94] = {"q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "Z", "A", "Q", "X", "S", "W", "C", "D", "E", "V", "F", "R", "B", "G", "T", "N", "H", "Y", "M", "J", "U", "K", "I", "L", "O", "P", "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "[", "}", "]", "|", "\\", ":", ";", "\"", "\'", "<", ",", ">", ".", "?", "/", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+	for(int x = 1 ; x > 0; x++){
+	srand(time(0));
+	random = rand() % 94;
+	cout << alpha[random];
+	random = rand() % 94;
+	cout << alpha[random];
+	random = rand() % 94;
+	cout << alpha[random];
+	random = rand() % 94;
+	cout << alpha[random];
+	random = rand() % 94;
+	cout << alpha[random];
+	random = rand() % 94;
+	cout << alpha[random];
+	random = rand() % 94;
+	cout << alpha[random];
+	random = rand() % 94;
+	cout << alpha[random] << "  *********    ";
+}
+}
